a4f7778510914e4b1e4f9561f479eaf86fcc6bcd
missopi <missopi@me.com>
2022-10-23 21:33:27 +0100

    Add code for hanged man

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,38 +11,49 @@ class Player
 end
 
 # class for designing and displaying the hanging man
-class HangingMan
-  attr_reader :hanging_man
+class Man
+  attr_reader :man
   
   def initialize
-    puts ' - - - -  '
-    puts ' |     O  '
-    puts ' |    /|\ '
-    puts ' |    / \ '
-    puts ' --       '
-    @hanging_man = ['\\', '/', '|', '\\', '/', 'O', '-', '-', '-', '-', '|', '|', '|', '-', '-' ]
+    puts ' '
+    puts ' --------  '
+    puts ' |      |  '
+    puts ' |      O  '
+    puts ' |     /|\ '
+    puts ' |     / \ '
+    puts ' '
+    @man = Array.new(8,' ')
+    @hanged_man = ['|', '-', 'O', '|', '/', '\\', '/', '\\', '|']
   end
   
-  def update_hanging_man(index, turn_taken)
-    hanging_man[index] = turn_taken
-    display_hanging_man(hanging_man)
+  def update_man(index, hanged_man)
+    man[index] = hanged_man
+    display_man(man)
   end
   
-  def display_hanging_man(hanging_man)
+  def display_man(man)
+    puts ' '
+    puts " #{man[1]}#{man[1]}#{man[1]}#{man[1]}#{man[1]}#{man[1]}#{man[1]}#{man[1]}  "
+    puts " #{man[0]}      #{man[8]}  "
+    puts " #{man[0]}      #{man[2]}  "
+    puts " #{man[0]}     #{man[4]}#{man[3]}#{man[5]} "
+    puts " #{man[0]}     #{man[6]} #{man[7]} "
+    puts ' '
   end
+end
+
+Man.new
   
+class Word
+  def initialize
   end
-  
-  class Word
-    def initialize
-    end
     
-    def display_blank_word(word)
-    end
+  def display_blank_word(word)
+  end
     
-    def update_word(hanging_man)
-    end
+  def update_word(hanging_man)
   end
+end
   
-  class Game
-  end
+class Game
+end
