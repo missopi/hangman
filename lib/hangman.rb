ccba3fc70aaef60b8a9368e3cc3a8f12cde0249d
missopi <missopi@me.com>
2022-10-23 21:03:46 +0100

    Add some more classes for displaying game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -10,3 +10,39 @@ class Player
   end
 end
 
+# class for designing and displaying the hanging man
+class HangingMan
+  attr_reader :hanging_man
+  
+  def initialize
+    puts ' - - - -  '
+    puts ' |     O  '
+    puts ' |    /|\ '
+    puts ' |    / \ '
+    puts ' --       '
+    @hanging_man = ['\\', '/', '|', '\\', '/', 'O', '-', '-', '-', '-', '|', '|', '|', '-', '-' ]
+  end
+  
+  def update_hanging_man(index, turn_taken)
+    hanging_man[index] = turn_taken
+    display_hanging_man(hanging_man)
+  end
+  
+  def display_hanging_man(hanging_man)
+  end
+  
+  end
+  
+  class Word
+    def initialize
+    end
+    
+    def display_blank_word(word)
+    end
+    
+    def update_word(hanging_man)
+    end
+  end
+  
+  class Game
+  end
