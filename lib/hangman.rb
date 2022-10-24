629ee4be8b2fef207302bcd8d8171dc2f70323d3
missopi <missopi@me.com>
2022-10-24 15:19:40 +0100

    Seperate rules from main game class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -55,22 +55,27 @@ class Word
     .join("")
   end
 end
+
+class Rules
+    def initialize
+        puts "\n============= Hangman =============\n\n"
+        puts "You will have 9 turns to guess the hidden word.\n\nEach wrong letter gets you a step closer to being hanged.\n\n"
+        puts ' --------  '  
+        puts ' |      |  '
+        puts ' |      O  '
+        puts ' |     /|\ '
+        puts ' |     / \ '
+        puts '---- '
+        puts "\nThe hidden word will be shown as a row of dashes representing each letter of the word.\n"
+        puts "\n_ _ _ _ _ _ _\n\nEach correct letter will be displayed in the word.\n"
+        puts "\n_ a _ _ _ a _\n\n========== End of Rules ==========\n\n"
+    end
+end
   
 class Game
   def initialize
-    puts "\n============= Hangman =============\n\n"
-    puts "You will have 9 turns to guess the hidden word.\n\nEach wrong letter gets you a step closer to being hanged.\n\n"
-    puts ' --------  '  
-    puts ' |      |  '
-    puts ' |      O  '
-    puts ' |     /|\ '
-    puts ' |     / \ '
-    puts '---- '
-    puts "\nThe hidden word will be shown as a row of dashes representing each letter of the word.\n"
-    puts "\n_ _ _ _ _ _ _\n\nEach correct letter will be displayed in the word.\n"
-    puts "\n_ a _ _ _ a _\n\n========== End of Rules ==========\n\n"
+    @rules = Rules.new
     @player = Player.new
-    
   end
 
   def move
