0194541f49d087adf8778c14490feeb2bbf85360
missopi <missopi@me.com>
2022-10-26 20:49:22 +0100

    Adjust display

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,6 +1,6 @@
 class Rules
     def initialize
-        puts "\n============= Hangman =============\n\n"
+        puts "\n======================= Hangman ========================\n\n"
         puts "You will have 9 turns to guess the hidden word.\n\nEach wrong letter gets you a step closer to being hanged.\n\n"
         puts ' --------  '  
         puts ' |      |  '
@@ -10,6 +10,6 @@ class Rules
         puts '---- '
         puts "\nThe hidden word will be shown as a row of dashes representing each letter of the word.\n"
         puts "\n_ _ _ _ _ _ _\n\nEach correct letter will be displayed in the word.\n"
-        puts "\n_ a _ _ _ a _\n\n========== End of Rules ==========\n\n"
+        puts "\n_ a _ _ _ a _\n\n==================== End of Rules ====================\n\n"
     end
 end
